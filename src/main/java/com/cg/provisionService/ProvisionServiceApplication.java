b88e3a43387ed712c7a12185e13e77c60440f8c7
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-22 22:39:38 +0530

    Update ProvisionServiceApplication.java

--- src/main/java/com/cg/provisionService/ProvisionServiceApplication.java
+++ src/main/java/com/cg/provisionService/ProvisionServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 @ComponentScan("com.cg.provisionService.*")
 public class ProvisionServiceApplication {
 
